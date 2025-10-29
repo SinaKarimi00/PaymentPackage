b6995b32f27241d1573b4c3a5e1dd72282f594d8
Sina Karimi <sinakarimi0938@gmail.com>
2025-10-29 09:45:52 +0330

    Editor logic Myket

---
+++ Editor/Logic/Myket/MyketStoreApplier.cs
@@ -0,0 +1,57 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Mirka.Payment.Editor.Editor.Tools;
+
+namespace Mirka.Payment.Editor.Editor.Logic.Myket
+{
+    public static class MyketStoreApplier
+    {
+        public static void Apply()
+        {
+            UnHideMyketFiles();
+            ReturnManifestDependencies();
+            SetSymbol();
+        }
+
+        private static void SetSymbol()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "MYKET");
+            Debug.Log("MYKET flag set.");
+
+            var currentSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+
+            if (currentSymbols.Contains("CAFEBAZAAR"))
+            {
+                currentSymbols = currentSymbols.Replace("CAFEBAZAAR", "");
+
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, currentSymbols);
+            }
+            else
+            {
+                Debug.LogWarning("CAFEBAZAAR flag not found.");
+            }
+        }
+
+        private static void ReturnManifestDependencies()
+        {
+            FileTools.SetFileHidden("Assets/Plugins/Android/AndroidManifest.xml", false);
+            FileTools.ToggleXmlCommentBlockByTagName("Assets/Plugins/Android/AndroidManifest.xml", "<queries>",
+                "</queries>",
+                false);
+            FileTools.ToggleXmlCommentBlockByTagName("Assets/Plugins/Android/AndroidManifest.xml",
+                "<receiver android:name=\"com.myket.util.IABReceiver\" android:exported=\"true\">",
+                "</receiver",
+                false);
+            FileTools.ToggleXmlCommentByKeyword("Assets/Plugins/Android/AndroidManifest.xml",
+                "ir.mservices.market.BILLING", false);
+        }
+
+        private static void UnHideMyketFiles()
+        {
+            FileTools.SetFileHidden("Packages/com.mirka.payment/Packages/.MyketIAB", false);
+            FileTools.SetFileHidden("Assets/Plugins/Android/.MyketIAB.jar", false);
+        }
+    }
+}
+#endif
