fcdbf232f6e931b097c0192ccfc1b59387b66e60
Sina Karimi <sinakarimi0938@gmail.com>
2025-10-29 09:45:39 +0330

    Editor logic Cafebazaar

---
+++ Editor/Logic/Cafebazzar/CafeBazaarStoreApplier.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Mirka.Payment.Editor.Editor.Tools;
+
+namespace Mirka.Payment.Editor
+{
+    public static class CafeBazaarStoreApplier
+    {
+        public static void Apply()
+        {
+            SetSymbol();
+            UnhideCafebazaarFiles();
+            ReturnBazaarToGradle();
+        }
+
+        private static void SetSymbol()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "CAFEBAZAAR");
+            Debug.Log("CAFEBAZAAR flag set.");
+
+            var currentSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+
+            if (currentSymbols.Contains("MYKET"))
+            {
+                currentSymbols = currentSymbols.Replace("MYKET", "");
+
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, currentSymbols);
+            }
+            else
+            {
+                Debug.LogWarning("MYKET flag not found.");
+            }
+        }
+
+        private static void ReturnBazaarToGradle()
+        {
+            FileTools.ToggleGradleCommentsByKeyword(
+                "Assets/Plugins/Android/mainTemplate.gradle",
+                "Bazaar",
+                false);
+        }
+
+        private static void UnhideCafebazaarFiles()
+        {
+            FileTools.SetFileHidden("Packages/com.mirka.payment/Packages/.Bazaar", false);
+        }
+    }
+}
+#endif
